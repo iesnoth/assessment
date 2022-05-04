7dae51104c597d322a35c2ec5b478ca5e566be26
Victoria B <103555407+iesnoth@users.noreply.github.com>
2022-05-04 02:32:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Obtain the img element, and assign it to the movie variable 
+let movie = document.querySelector("#movie")
+
+// Obtain the 'change-border' button, and assign it to the changeBorder variable
+let changeBorder = document.querySelector("#change-border")
+
+
+//DO NOT CHANGE THE FOLLOWING CODE
+changeBorder.addEventListener("click", function() {
+  if (movie.style.borderRadius == "0px") {
+    movie.style.borderRadius = "25px"
+  }
+  else {
+    movie.style.borderRadius = "0px"
+  }
+})
